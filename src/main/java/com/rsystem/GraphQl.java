e936756b08834f669f7557abfb053fe41f25fff7
NishuSharma131 <nishu.sharma131@gmail.com>
2023-06-20 18:18:41 +0530

    Initial commit

---
+++ src/main/java/com/rsystem/GraphQl.java
@@ -0,0 +1,14 @@
+package com.rsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQl {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        SpringApplication.run(GraphQl.class, args);
+
+
+    }
+}
